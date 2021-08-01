d05eff2f78eca73220a5924898a4da381ee77d14
Nadeesha Pathiraja <pdncpathiraja95@gmail.com>
2021-08-01 22:16:22 +0530

    Add base project

---
+++ login.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+
+<body>
+
+    <section class="vh-800 gradient-custom">
+        <div class="container py-5 h-800">
+            <div class="row justify-content-center align-items-center h-500">
+                <div class=" body col-12 col-lg-9 col-xl-7">
+                    <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+
+                        <div class="card-body p-4 p-md-5">
+
+                            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 reg_title">Sign In</h3>
+                            <form method="post" action="#">
+
+
+
+                                <div class="row">
+                                    <div class="col-md-12 mb-4 pb-2">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Email: </h6>
+                                            <input type="email" id="emailAddress"
+                                                class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-12 mb-4 pb-2">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Password: </h6>
+                                            <input type="password" id="password" class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <input class="btn btn-danger btn-lg btn_cancle" value="CANCLE" />
+
+                                    </div>
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <input class="btn btn-primary btn-lg btn_log" type="submit" value="SIGN UP" />
+
+                                    </div>
+                                </div>
+
+
+                            </form>
+                        </div>
+                    </div>
+                    <div class="mt-4 pt-5">
+
+                    </div>
+                    <div class="mt-4 pt-5">
+
+                    </div>
+                    <div class="mt-4 pt-2">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
---
+++ register.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="assets/styles/style.css">
+</head>
+
+<body>
+
+    <section class="vh-200 gradient-custom">
+        <div class="container py-5 h-100">
+            <div class="row justify-content-center align-items-center h-100">
+                <div class=" body col-12 col-lg-9 col-xl-7">
+                    <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
+
+                        <div class="card-body p-4 p-md-5">
+
+                            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5 reg_title">Sign Up</h3>
+                            <form method="post" action="#">
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">First Name: </h6>
+                                            <input type="text" id="firstName" class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6 mb-4">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Last Name: </h6>
+                                            <input type="text" id="lastName" class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 d-flex align-items-center">
+
+                                        <div class="form-outline datepicker w-100">
+                                            <h6 class="mb-2 pb-1">Birthday: </h6>
+                                            <input type="text" class="form-control form-control-lg" id="birthdayDate" />
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6 mb-4">
+
+                                        <h6 class="mb-2 pb-1">Gender: </h6>
+
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                id="femaleGender" value="option1" checked />
+                                            <label class="form-check-label" for="femaleGender">Female</label>
+                                        </div>
+
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                id="maleGender" value="option2" />
+                                            <label class="form-check-label" for="maleGender">Male</label>
+                                        </div>
+
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="radio" name="inlineRadioOptions"
+                                                id="otherGender" value="option3" />
+                                            <label class="form-check-label" for="otherGender">Other</label>
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-outline pb-3">
+                                        <h6 class="mb-2 pb-1">Address: </h6>
+                                        <input type="text" id="address" class="form-control form-control-lg" />
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Email: </h6>
+                                            <input type="email" id="emailAddress"
+                                                class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Phone Number: </h6>
+                                            <input type="tel" id="phoneNumber" class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Password: </h6>
+                                            <input type="password" id="password" class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <div class="form-outline">
+                                            <h6 class="mb-2 pb-1">Confirm Password: </h6>
+                                            <input type="password" id="confirm_password" name="confirm_password"
+                                                class="form-control form-control-lg" />
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <input class="btn btn-danger btn-lg btn_cancle" value="CANCLE" />
+
+                                    </div>
+                                    <div class="col-md-6 mb-4 pb-2">
+
+                                        <input class="btn btn-primary btn-lg btn_reg" type="submit" value="SIGN UP" />
+
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
